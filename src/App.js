eda8b94cd78d424522272881f7b566c94bb6bb56
Thomas Lombardi <thomas.lombardi@nokia.com>
2017-03-22 09:52:18 -0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, browserHistory } from 'react-router-dom';
 
 import Ingredients from './components/Ingredients';
-import Button from './components/Button';
-// import { Recipes } from './components/Recipes';
+import Recipes from './components/Recipes';
 import logo from './logo.svg';
 import './App.css';
 
 
 const Home = () => (
-    <div><h2>You are Home</h2></div>
+    <div>
+        <h2>You are Home</h2>
+    </div>
 )
 
-const Recipes = () => (
-    <div><h2>Recipes</h2></div>
+const Cook = () => (
+    <div className="cookie-jar">
+        <h2>COOKIE</h2>
+        <Link to="/cookie/choco">Chocolate Chip</Link>
+        <Link to="/cookie/sugar">Sugar</Link>
+        <Link to="/cookie/snickerdoodle">Snickerdoodle</Link>
+        <Route path="/cookie/:section" render={({match}) => <h2>{match.params.section}</h2>} />
+    </div>
 )
+//
+// const Recipes = () => (
+//     <div><h2>Recipes</h2></div>
+// )
 
 
 class App extends Component {
@@ -23,34 +34,29 @@ class App extends Component {
 
     render() {
         return (
-            
 
-            <div className="App">
-            <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-            </div>
 
             <div>
-            <Router>
-            <div>
-            <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/recipes">Recipes</Link></li>
-            <li><Link to="/ingredients">Ingredients</Link></li>
-            </ul>
+                <Router history={browserHistory}>
+                    <div>
+                        <div className="header">
+                            <ul>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/recipes">Recipes</Link></li>
+                                <li><Link to="/ingredients">Ingredients</Link></li>
+                                <li><Link to="/cookie">Cookie</Link></li>
+                            </ul>
+                        </div>
 
-            <hr />
+                        <hr />
 
-            <Route exact path="/" component={Home} />
-            <Route path="/recipes" component={Recipes} />
-            <Route path="/ingredients" component={Ingredients} />
+                        <Route exact path="/" component={Home} />
+                        <Route component={Recipes} />
+                        <Route path="/ingredients" component={Ingredients} />
+                        <Route path="/cookie" component={Cook} />
 
-            </div>
-            </Router>
-            <Button color="red" />
-
-            </div>
+                    </div>
+                </Router>
             </div>
 
 
